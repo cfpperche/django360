55d490e048cfb4052c71d628f85b92a7042861d3
cfpperche <ebugfix@gmail.com>
2015-06-03 11:28:37 -0300

    teste 0.0.1

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -1,12 +1,25 @@
 
 MESSAGE=$1
 
+echo +++++ GRUNT CLEAN +++++
 grunt clean
+
+echo +++++ GRUNT BUILD +++++
 grunt build
+
+echo +++++ GIT ADD --ALL +++++
 git add --all
+
+echo +++++ GIT COMMIT +++++
 git commit -m 'teste 0.0.1'
+
+echo +++++ GIT DELETE GH_PAGES +++++
 git push origin --delete gh-pages
+
+echo +++++ GIT PUSH +++++
 git push origin master
+
+echo +++++ GIT PUSH GH_PAGES +++++
 git subtree push --prefix dist origin gh-pages
 
 
